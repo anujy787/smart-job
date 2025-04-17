5fb93343762d0bf6313670813ba2d9e45bdcecdc
Anirudh Maheshwari <maheshwari.ani@northeastern.edu>
2025-04-17 16:07:41 -0400

    initial code changes

---
+++ smartjob/src/test/java/edu/neu/csye7374/smartjob/SmartjobApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.csye7374.smartjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
